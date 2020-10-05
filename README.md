7acd38dbf70f8c047f77ffdb3ed49cbec4bc24fb
Benjamin Widawski <benjamin.widaw@gmail.com>
2020-10-05 17:45:18 +0200

    Add 'to' option and better algorithm

---
+++ README.md
@@ -0,0 +1,25 @@
+# Owncloud Git Sync
+
+Copies the content of the current directory to another directory, while excluding the folders and files in the gitignores. Also working with submodules.
+
+It requires Cygwin installed to work
+```(location : C:\Program\ Files\Git\usr\bin\sh.exe)```
+
+## Install dependencies
+```
+$ npm install
+```
+
+## Run
+```
+$ cd PATH_TO_FOLDER_TO_EXPORT
+$ node PATH_TO_OWNCLOUD_GIT_SYNC/app.js --to=PATH_TO_FOLDER_OUTPUT
+```
+
+## Example
+```
+$ cd /c/Users/me/Projects/MyAwesomeProgram
+$ node /c/Users/me/OwncloudGitSync/app.js --to=/c/Users/me/SavedProjects/
+```
+This will export _MyAwesomeProgram_ to **/c/Users/me/SavedProjects/MyAwesomeProgram** along with its submodules
+
--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const exec = require('await-exec')
-
-const ROOT_FROM_FOLDER = "/c/Users/benja/Documents/Projects/";
-const ROOT_TO_FOLDER = "/c/Users/benja/ownCloud/Projects/";
+const args = require("args-parser")(process.argv)
 
 async function run(cmd) {
     return await exec('"C:\\Program\ Files\\Git\\usr\\bin\\sh.exe" -c "' + cmd + '"')
 }
 
 (async () => {
-    
     const currentFolder = (await exec('pwd'))
         .stdout
         .replace(/(\r\n|\n|\r)/gm,"")
+    const ROOT_FROM_FOLDER = currentFolder.replace(currentFolder.split('/').slice(-1)[0], "")
+    const ROOT_TO_FOLDER = args.to.replace("C:", "/c")
 
     // Find gitignores
     const findResult = await exec('find ' + currentFolder + ' -name ".gitignore"')
@@ -19,7 +18,7 @@ async function run(cmd) {
     folders.pop()
     
     const promises = folders.map(folder => {
-        const trimmedFolder = folder.replace(ROOT_FROM_FOLDER,"")
+        const trimmedFolder = folder.replace(ROOT_FROM_FOLDER, "")
         const fromPath = ROOT_FROM_FOLDER + trimmedFolder
         const toPath = ROOT_TO_FOLDER + trimmedFolder
         return run('mkdir -p ' + ROOT_TO_FOLDER + trimmedFolder + ';'
